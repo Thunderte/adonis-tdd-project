1ffe3ae8a90745b93e4892741eb5b04f759e6339
Jonathan Santos <jonathan.santos2@estudante.ifms.edu.br>
2024-05-03 12:59:23 -0400

    feature: adicionando rollback na database após os testes

--- tests/functional/users/login.spec.ts
+++ tests/functional/users/login.spec.ts
@@ -1,8 +1,15 @@
 import { test } from '@japa/runner'
 import UserFactory from 'Database/factories/UserFactory'
+import Database from '@ioc:Adonis/Lucid/Database';
 
-test.group('Login de usuário', () => {
-  test('Deve logar um usuário com sucesso', async ({ assert, client }) => {
+test.group('Login de usuário', (group) => {
+  //Desfazendo alterações no banco de dados
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+    return () => Database.rollbackGlobalTransaction()
+  })
+  
+  test('Deve logar um usuário com sucesso', async ({ assert, client }, group) => {
   //criando um usuário aleatório
   const user = await UserFactory.create();
 
@@ -15,5 +22,17 @@ test.group('Login de usuário', () => {
   //fazendo o teste que verifica se retornou um token
   response.assertStatus(200);
   assert.exists(response.response.body.token);
+
+})
+test('Deve retornar erro ao tentar logar com credenciais inválidas', async ({ assert, client }) => {
+  //realizando login
+  const response = await client.post('/signin').form({
+    email: "teststdskd@gmail.com",
+    password: "123456"
+  })
+
+  //fazendo o teste que verifica se retornou um token
+  response.assertStatus(400);
+  assert.equal(response.response.body.code, 'BAD_REQUEST_EXCEPTION');
 })
 })
