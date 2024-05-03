31eb84520aceb9b1f2e22078e6b16e2f9277d77a
Jonathan Santos <jonathan.santos2@estudante.ifms.edu.br>
2024-05-03 11:39:39 -0400

    fix: Tranferindo teste unitário para pasta certa

--- tests/unit/users/create.spec.ts
+++ tests/unit/users/create.spec.ts
@@ -1,14 +1,21 @@
 import { test } from '@japa/runner'
 import UserService from 'App/Services/UserService'
+import Database from '@ioc:Adonis/Lucid/Database';
 
-test.group('Criar usuário', () => {
+test.group('Criar usuário', (group) => {
+  //Desfazendo alterações no banco de dados
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+    return () => Database.rollbackGlobalTransaction()
+  })
+
   test('Deve criar um usuário com sucesso', async ({ assert }) => {
 
     //gerando um nome aleatório
     const username = Math.random().toString(36).substring(7)
 
     //gerando email aleatório
-    const email = Math.random().toString(36).substring(7) + 'teste.com';
+    const email = Math.random().toString(36).substring(7) + '@teste.com';
 
     const response = await UserService.createUser(username, email, '12345678')
 
